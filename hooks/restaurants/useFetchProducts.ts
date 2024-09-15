bc14cbabca734f4231134c5a01f713413c152af7
DEV RAAJ <Dev Raj Singh>
2024-09-15 22:18:53 +0530

    resolve-bug

--- hooks/restaurants/useFetchProducts.ts
+++ hooks/restaurants/useFetchProducts.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "react-query";
-import { getProductsByRestaurant } from "@/utils/products/products";
+import { getRestaurants } from "@/utils/restaurants/restaurants";
 
 // fetch restaurant products
 export const useFetchProducts = (restaurantId: string) => {
-    return useQuery('products', () => getProductsByRestaurant(restaurantId));
+    return useQuery('products', () => getRestaurants());
 }
