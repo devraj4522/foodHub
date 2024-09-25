130e2a9207aae23995015834a05d8aaf210936b5
DEV RAAJ <rehansingh.4522@gmail.com>
2024-09-26 04:52:19 +0530

    debug

--- server/controllers/menuItemController.ts
+++ server/controllers/menuItemController.ts
@@ -1,28 +1,28 @@
-import { IMenuItem } from "@/types/Restaurant";
-import {
-  getMenuItemById,
-  createMenuItem,
-  updateMenuItem,
-  deleteMenuItem,
-  getMenuItemsByRestaurant
-} from '../services/menuItemService';
+// import { IMenuItem } from "@/types/Restaurant";
+// import {
+//   getMenuItemById,
+//   createMenuItem,
+//   updateMenuItem,
+//   deleteMenuItem,
+//   getMenuItemsByRestaurant
+// } from '../services/menuItemService';
 
-export async function getMenuItemByIdController(id: string) {
-  return getMenuItemById(id);
-}
+// export async function getMenuItemByIdController(id: string) {
+//   return getMenuItemById(id);
+// }
 
-export async function createMenuItemController(data: IMenuItem) {
-  return createMenuItem(data);
-}
+// export async function createMenuItemController(data: IMenuItem) {
+//   return createMenuItem(data);
+// }
 
-export async function updateMenuItemController(id: string, data: Partial<Omit<IMenuItem, 'id'>>) {
-  return updateMenuItem(id, data);
-}
+// export async function updateMenuItemController(id: string, data: Partial<Omit<IMenuItem, 'id'>>) {
+//   return updateMenuItem(id, data);
+// }
 
-export async function deleteMenuItemController(id: string) {
-  return deleteMenuItem(id);
-}
+// export async function deleteMenuItemController(id: string) {
+//   return deleteMenuItem(id);
+// }
 
-export async function getMenuItemsByRestaurantController(restaurantId: string) {
-  return getMenuItemsByRestaurant(restaurantId);
-}
+// export async function getMenuItemsByRestaurantController(restaurantId: string) {
+//   return getMenuItemsByRestaurant(restaurantId);
+// }
--- server/models/MenuItem.ts
+++ server/models/MenuItem.ts
@@ -1,50 +1,50 @@
-import { prisma } from "@/server/lib/prisma";
-import { IMenuItem } from "@/types/Restaurant";
+// import { prisma } from "@/server/lib/prisma";
+// import { IMenuItem } from "@/types/Restaurant";
 
-export class MenuItem {
-  static async getMenuItemById(id: string) {
-    return prisma.menuItem.findUnique({
-      where: { id },
-    });
-  }
+// export class MenuItem {
+//   static async getMenuItemById(id: string) {
+//     return prisma.menuItem.findUnique({
+//       where: { id },
+//     });
+//   }
 
-  static async createMenuItem(data: IMenuItem) {
-    const { category, restaurant, ...menuItemData } = data;
-    return prisma.menuItem.create({
-      data: {
-        ...menuItemData,
-        categoryId: category.id,
-        restaurantId: restaurant.id,
-      },
-    });
-  }
+//   static async createMenuItem(data: IMenuItem) {
+//     const { category, restaurant, ...menuItemData } = data;
+//     return prisma.menuItem.create({
+//       data: {
+//         ...menuItemData,
+//         categoryId: category.id,
+//         restaurantId: restaurant.id,
+//       },
+//     });
+//   }
 
-  // static async updateMenuItem(id: string, data: Partial<Omit<IMenuItem, 'id'>>) {
-  //   const { category, restaurant, ...updateData } = data;
+//   // static async updateMenuItem(id: string, data: Partial<Omit<IMenuItem, 'id'>>) {
+//   //   const { category, restaurant, ...updateData } = data;
 
-  //   if (category?.id) {
-  //     updateData.categoryId = category.id;
-  //   }
+//   //   if (category?.id) {
+//   //     updateData.categoryId = category.id;
+//   //   }
 
-  //   if (restaurant?.id) {
-  //     updateData.restaurantId = restaurant.id;
-  //   }
+//   //   if (restaurant?.id) {
+//   //     updateData.restaurantId = restaurant.id;
+//   //   }
 
-  //   return prisma.menuItem.update({
-  //     where: { id },
-  //     data: updateData,
-  //   });
-  // }
+//   //   return prisma.menuItem.update({
+//   //     where: { id },
+//   //     data: updateData,
+//   //   });
+//   // }
 
-  static async deleteMenuItem(id: string) {
-    return prisma.menuItem.delete({
-      where: { id },
-    });
-  }
+//   static async deleteMenuItem(id: string) {
+//     return prisma.menuItem.delete({
+//       where: { id },
+//     });
+//   }
 
-  static async getMenuItemsByRestaurant(restaurantId: string) {
-    return prisma.menuItem.findMany({
-      where: { restaurantId },
-    });
-  }
-}
+//   static async getMenuItemsByRestaurant(restaurantId: string) {
+//     return prisma.menuItem.findMany({
+//       where: { restaurantId },
+//     });
+//   }
+// }
