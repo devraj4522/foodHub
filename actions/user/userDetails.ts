fff877ec47f44ada9d2ea990a27f83481dbaebf4
DEV RAAJ <rehansingh.4522@gmail.com>
2024-09-26 04:24:14 +0530

    pushing with backend

--- actions/user/userDetails.ts
+++ actions/user/userDetails.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { UserDetails } from "@/types";
 import { ICreateUserInput } from "@/types/User";
 
-export const updateUserDetails =  async (userData: ICreateUserInput & { id: string }) => {
+export const updateUserDetails = async (userData: Omit<ICreateUserInput, 'otpCode'> & { id: string }) => {
     try {
         return await axios.put("/api/user", userData);
     } catch (error) {
