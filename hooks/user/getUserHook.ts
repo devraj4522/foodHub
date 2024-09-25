ab683abad801b02f6fd0ec3be168a89683940b1e
DEV RAAJ <rehansingh.4522@gmail.com>
2024-09-26 04:43:09 +0530

    pushing with backend

--- hooks/user/getUserHook.ts
+++ hooks/user/getUserHook.ts
@@ -4,17 +4,26 @@ import { useRecoilState } from "recoil";
 import { userAtom } from "@/recoil/atoms/userAtom";
 import { UserDetails } from "@/types";
 
-export const useGetUserHook = () => {
-  const [user, setUser] = useRecoilState(userAtom);
-  const { data, isLoading, error } = useQuery("user", getUser, {
-    onSuccess: (data) => {
-      if (data?.user) {
-        setUser(data.user as UserDetails);
-      }
-    },
-    staleTime: Infinity,
-    cacheTime: Infinity,
-  });
+// export const useGetUserHook = () => {
+//   const [user, setUser] = useRecoilState(userAtom);
+//   const { data, isLoading, error } = useQuery("user", getUser, {
+//     onSuccess: (data) => {
+//       if (data && data.user) {
+//         const userDetails: UserDetails = {
+//           ...data.user,
+//           address: [data.user?.address || ''],
+//           phone: data.user?.phone || '',
+//           city: data.user?.city || '',
+//           state: data.user?.state || '',
+//           pinCode: data.user?.pinCode || '',
+//           isLoggedIn: data.user?.isLoggedIn || false
+//         };
+//         setUser(userDetails);
+//       }
+//     },
+//     staleTime: Infinity,
+//     cacheTime: Infinity,
+//   });
 
-  return { user, isLoading, error };
-};
+//   return { user, isLoading, error };
+// };
