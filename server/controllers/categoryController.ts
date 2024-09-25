0df415a5c1a6c450d30bc25c59b51bd2353c8b2e
DEV RAAJ <rehansingh.4522@gmail.com>
2024-09-26 04:56:15 +0530

    pushing with backend

--- server/controllers/categoryController.ts
+++ server/controllers/categoryController.ts
@@ -1,23 +1,23 @@
-import { CategoryData } from '@/types/Restaurant';
-import {
-  getCategoryById,
-  getCategoriesByRestaurantId,
-  createCategory,
-  updateCategory
-} from '../services/categoryService';
+// import { CategoryData } from '@/types/Restaurant';
+// import {
+//   getCategoryById,
+//   getCategoriesByRestaurantId,
+//   createCategory,
+//   updateCategory
+// } from '../services/categoryService';
 
-export async function getCategoryByIdController(id: string) {
-  return getCategoryById(id);
-}
+// export async function getCategoryByIdController(id: string) {
+//   return getCategoryById(id);
+// }
 
-export async function getCategoriesByRestaurantIdController(restaurantId: string) {
-  return getCategoriesByRestaurantId(restaurantId);
-}
+// export async function getCategoriesByRestaurantIdController(restaurantId: string) {
+//   return getCategoriesByRestaurantId(restaurantId);
+// }
 
-export async function createCategoryController(data: Omit<CategoryData, 'id' | 'menuItems'>) {
-  return createCategory(data);
-}
+// export async function createCategoryController(data: Omit<CategoryData, 'id' | 'menuItems'>) {
+//   return createCategory(data);
+// }
 
-export async function updateCategoryController(id: string, data: Partial<Omit<CategoryData, 'id' | 'menuItems'>>) {
-  return updateCategory(id, data);
-}
+// export async function updateCategoryController(id: string, data: Partial<Omit<CategoryData, 'id' | 'menuItems'>>) {
+//   return updateCategory(id, data);
+// }
