135e0e6efa2597e03adff41d65a080dc9f1ec621
DEV RAAJ <rehansingh.4522@gmail.com>
2024-09-27 12:32:08 +0530

    error in fetch actions

--- actions/restaurant/index.ts
+++ actions/restaurant/index.ts
@@ -1,11 +1,6 @@
 export async function getAllRestaurants() {
   try {
-    let response;
-    if (process.env.NODE_ENV === 'development') {
-      response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/restaurant/restaurants`)
-    } else {
-      response = await fetch(`/api/restaurant/restaurants`)
-    }
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/restaurant/restaurants`)
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
@@ -13,6 +8,6 @@ export async function getAllRestaurants() {
     return data;
   } catch (error) {
     console.error("error in fetch actions", error);
-    // throw error;
+    throw error;
   }
 }
