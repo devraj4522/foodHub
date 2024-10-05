c8145114b3961961c5b735c5c839463a03ddfc42
DEV RAAJ <rehansingh.4522@gmail.com>
2024-10-05 11:47:10 +0530

    active order

--- hooks/user/auth/useVerifyJwtToken.ts
+++ hooks/user/auth/useVerifyJwtToken.ts
@@ -2,15 +2,18 @@ import { useEffect } from 'react';
 import { verifyToken } from '@/actions/user/auth';
 import { useRecoilState } from 'recoil';
 import { userAtom } from '@/recoil/atoms/userAtom';
+import { userSavedAddressAtom } from '@/recoil/atoms/locationAtom';
 
 export const useVerifyJwtToken = () => {
   const [user, setUser] = useRecoilState(userAtom);
-
+  const [, setSavedAddress] = useRecoilState(userSavedAddressAtom)
+  
   useEffect(() => {
     const verifyUserToken = async () => {
         try {
           const userData = await verifyToken();
           setUser(userData.user);
+          setSavedAddress(userData.user.address)
         } catch (error) {
           // console.error('Token verification failed:', error);
           setUser(null); // Clear user data if verification fails
