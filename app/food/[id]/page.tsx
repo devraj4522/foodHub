31ff106356a9a3eb4585295a82d7ebda5fb3299b
DEV RAAJ <rehansingh.4522@gmail.com>
2024-10-18 01:16:38 +0530

    suspense in server comp

--- app/food/[id]/page.tsx
+++ app/food/[id]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 import { getMenuItemById } from "@/actions/food";
 import FoodItemDetails from "./_components/Food/FoodItemDetails";
 import { Suspense } from "react";
+import { Skeleton } from "@nextui-org/skeleton";
 
 export default async function FoodItemPage({params}: {params: {id: string}}) {
   if (!params.id || typeof params.id !== 'string') {
@@ -10,7 +11,9 @@ export default async function FoodItemPage({params}: {params: {id: string}}) {
   const foodItem = await getMenuItemById(params.id);
   return (
     <div>
-      <Suspense fallback={<div>Loading...</div>} >
+      <Suspense fallback={
+        <Skeleton className=" w-screen"  />
+      } >
         <FoodItemDetails foodItem={foodItem} />
       </Suspense>
     </div>
