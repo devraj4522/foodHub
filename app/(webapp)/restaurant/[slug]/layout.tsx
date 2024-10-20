290e3596a56104976173e1e9ec2e6699be9ab560
DEV RAAJ <rehansingh.4522@gmail.com>
2024-10-20 23:46:16 +0530

    restaurnt app

--- app/(webapp)/restaurant/[slug]/layout.tsx
+++ app/(webapp)/restaurant/[slug]/layout.tsx
@@ -1,4 +1,3 @@
-import { QueryClient, QueryClientProvider } from "react-query";
 import { Suspense } from "react";
 
 export default function RootLayout({
@@ -6,14 +5,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
       <Suspense fallback={<div className="h-screen absolute w-screen items-center  justify-center">
         <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-black"></div>
       </div>}> 
       {children}
       </Suspense>
-    </QueryClientProvider>
   );
 }
