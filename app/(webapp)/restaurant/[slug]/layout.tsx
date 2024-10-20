f30984e22374cc3877334e9c5d85c81bfe041dc5
DEV RAAJ <rehansingh.4522@gmail.com>
2024-10-21 00:18:35 +0530

    pushing

--- app/(webapp)/restaurant/[slug]/layout.tsx
+++ app/(webapp)/restaurant/[slug]/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
       <Suspense fallback={<div className="h-screen w-screen items-center flex justify-center">
-        <div className="animate-spin rounded-full md:h-8 w-8 border border-black block"></div>
+        <div className="animate-spin rounded-full h-8 w-8 border-4 border-black border-t-transparent"></div>
       </div>}> 
       {children}
       </Suspense>
